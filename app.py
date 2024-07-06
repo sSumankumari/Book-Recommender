14893115d580c5c6489d2a530705b653cf353c4f
Suman Kumari <123852425+sSumankumari@users.noreply.github.com>
2024-07-06 12:13:12 +0530

    Modified recommendation funtion

--- app.py
+++ app.py
@@ -8,19 +8,33 @@ pt = pickle.load(open('pt.pkl', 'rb'))
 books = pickle.load(open('books.pkl', 'rb'))
 similarity_scores = pickle.load(open('similarity_scores.pkl', 'rb'))
 
-def recommend_books(book_name):
-    index = np.where(pt.index == book_name)[0][0]
-    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
 
-    recommended_books = []
-    for i in similar_items:
+def recommend(book_name):
+    # Returns the numerical index for the book_name
+    index = np.where(pt.index == book_name)[0][0]
+
+    # Sorts the similarities for the book_name in descending order
+    similar_books = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:6]
+
+    # To return result in list format
+    data = []
+
+    for idx, similarity in similar_books:
         item = {}
-        temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+        # Get the book details by index
+        temp_df = books[books['Book-Title'] == pt.index[idx]]
+
+        # Add all relevant details to the result item
         item['title'] = temp_df.drop_duplicates('Book-Title')['Book-Title'].values[0]
         item['author'] = temp_df.drop_duplicates('Book-Title')['Book-Author'].values[0]
+        item['publisher'] = temp_df.drop_duplicates('Book-Title')['Publisher'].values[0]
+        item['publication_year'] = temp_df.drop_duplicates('Book-Title')['Year-Of-Publication'].values[0]
         item['image'] = temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values[0]
-        recommended_books.append(item)
-    return recommended_books
+
+        data.append(item)
+
+    return data
+
 
 st.title("Book Recommender System")
 
@@ -37,11 +51,13 @@ st.header("Recommend Books")
 book_name = st.text_input("Enter a book name")
 if st.button("Recommend"):
     try:
-        recommendations = recommend_books(book_name)
+        recommendations = recommend(book_name)
         for book in recommendations:
             st.image(book['image'], width=100)
             st.write(book['title'])
             st.write(f"Author: {book['author']}")
+            st.write(f"Publisher: {book['publisher']}")
+            st.write(f"Publication Year: {book['publication_year']}")
     except IndexError:
         st.write("Book not found in the dataset.")
 
